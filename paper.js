d8ea663369d6b5faae0e3fa3a83b6ed5098a4e04
aarul2008 <78950142+aarul2008@users.noreply.github.com>
2021-05-26 22:05:58 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,43 @@
+class paper
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+           restitution : 0.3,
+            friction : 0,
+            density : 1.2			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+        this.image = loadImage("paper.png")
+		this.body=Bodies.circle(this.x,this.y,(this.r-20)/2,options);
+ 		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+					
+		var paperPos=this.body.position;  
+			push()
+			translate(paperPos.x, paperPos.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+            strokeWeight(4)
+			fill(255,0,255)
+			circle(this.body.position.x,this.body.position.y,this.radius);
+			pop()
+			
+	}
+
+}
+ function keyPressed() {
+if(keyCode === UP_ARROW){
+
+Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:130,y:-145})
+ 
+
+}
+ }
